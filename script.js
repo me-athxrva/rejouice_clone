c9dbf0686bf74875713ccd8f7ed5233d7fe70572
atharva-4 <atharvadeore04@gmail.com>
2024-11-15 23:45:26 +0530

    added preloader

--- script.js
+++ script.js
@@ -10,6 +10,41 @@ var reel_video = document.getElementById("play-reel-video");
 var bg_video = document.getElementById("bg_video");
 var menu_take_seat_btn = document.getElementById("take_seat_btn"); 
 
+
+function locomotive_st(){
+    // locomotive scrolltrigger 
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
+
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+}
+
+
+
 // universal cursor function
 function cursor(pg_src){
     pg_src.addEventListener("mousemove",function(pos){
@@ -65,7 +100,7 @@ function menu_toggle(up_down){
     } else {
         gsap.to(".menu-content",{
             top: "-100%",
-            duration: 1.2,
+            duration: 1,
             ease: "power1.inOut"
         })
         menu_video.pause()
@@ -79,12 +114,13 @@ function menu_timeline(action){
     });
 
     menu_tl.fromTo("#menu_video",{
-        scale: "0.1",
+        scale: 0,
         opacity: 0,
     },{
-        scale: "1",
+        scale: 1,
         opacity: 1,
         duration: 1.5,
+        ease: "power"
     })
 
     menu_tl.fromTo(".menu-content-middle ul li a",{
@@ -109,7 +145,6 @@ function menu_timeline(action){
     },{
         x: "0%",
         duration: 1,
-        ease: "power1.inOut"
     },"-=1")
 
     menu_tl.fromTo(".menu-content-bottom h4",{
@@ -117,13 +152,44 @@ function menu_timeline(action){
     },{
         opacity: 1,
         duration: 0.5
-    },"-=1")
+    },"-=0.8")
 
     if(action=="play"){
         menu_tl.play();
     } else {
-        menu_tl.reverse(2);
+        menu_tl.reverse(true);
     }
+}
+
+// home_timeline
+function home_timeline(){
+    const home_tl = gsap.timeline({
+        paused: true
+    });
+
+    home_tl.to(".text span",{
+        y: 0,
+        opacity: 1,
+        duration: 0.5,
+        ease: "power3.inOut",
+        stagger: 0.2,
+        onComplete: () => {
+            home_tl.to(".text span",{
+                opacity: 0,
+                duration: 0.5,
+                ease: "power3.inOut",
+                stagger: 0.2,
+                delay: 0.25
+            })
+            home_tl.to(".preloader",{
+                opacity: 0,
+                duration: 1,
+                ease: "power1.inOut",
+                delay: 0.25
+            })
+        }
+    })
+    home_tl.play()
 }
 
 // page 1 all functions and calls
@@ -187,5 +253,10 @@ for (let i = 0; i < menu_span.length; i++) {
 }
 
 // page1 function calls
+locomotive_st()
 cursor(page_1)
 menu_btn_animation()
+
+addEventListener("DOMContentLoaded",function(){
+    home_timeline();
+})
